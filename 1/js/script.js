f7de6e8eeb6f3506cfb56911cb5c6a0869a9f2b7
masasaeki <masaakisaeki@gmail.com>
2021-07-04 21:30:17 +0900

    インデントを修正

--- 1/js/script.js
+++ 1/js/script.js
@@ -21,11 +21,11 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
 function setFadeElement(){
 	var windowH = $(window).height();	//ウィンドウの高さを取得
 	var scroll = $(window).scrollTop(); //スクロール値を取得
-    
+
     //出現範囲の指定
 	var contentsTop = Math.round($('#blog').offset().top);	//要素までの高さを取得
 	var contentsH = $('#blog').outerHeight(true);	//要素の高さを取得
-    
+
     //2つ目の出現範囲の指定※任意
 	var contentsTop2 = Math.round($('#footer').offset().top);	//要素までの高さを取得
 	var contentsH2 = $('#footer').outerHeight(true);//要素の高さを取得
@@ -36,14 +36,14 @@ function setFadeElement(){
 		$("#page-top").removeClass("DownMove");   //DownMoveを削除
 		$(".hide-btn").removeClass("hide-btn");	  //hide-btnを削除
 	}//2つ目の出現範囲に入ったかどうかをチェック※任意
-   else if(scroll+windowH >= contentsTop2 && scroll+windowH <= contentsTop2+contentsH2){       
+	else if(scroll+windowH >= contentsTop2 && scroll+windowH <= contentsTop2+contentsH2){
 		$("#page-top").addClass("UpMove");    //入っていたらUpMoveをクラス追加
 		$("#page-top").removeClass("DownMove");   //DownMoveを削除
 	}//それ以外は
-    else{
+  else{
         if(!$(".hide-btn").length){				//サイト表示時にDownMoveクラスを一瞬付与させないためのクラス付け。hide-btnがなければ下記の動作を行う
         $("#page-top").addClass("DownMove");  //DownMoveをクラス追加
-		$("#page-top").removeClass("UpMove"); //UpMoveを削除	
+		$("#page-top").removeClass("UpMove"); //UpMoveを削除
         }
 	}
 }
@@ -110,7 +110,7 @@ function fadeAnime(){
 		$(this).removeClass('fadeIn');// 画面外に出たらfadeInというクラス名を外す
 		}
 		});
-    
+
     // 4-4 ボンッ（拡大）
 	$('.zoomOutTrigger').each(function(){ //zoomOutTriggerというクラス名が
 		var elemPos = $(this).offset().top;//要素上の
@@ -121,8 +121,8 @@ function fadeAnime(){
 		}else{
 		$(this).removeClass('zoomOut');// 画面外に出たらzoomOutというクラス名を外す
 		}
-		});	
-    
+		});
+
 }
 
 /*===========================================================*/
@@ -131,7 +131,7 @@ function fadeAnime(){
 
 function slideAnime(){
 	//====左右に動くアニメーションここから===
-		$('.leftAnime').each(function(){ 
+		$('.leftAnime').each(function(){
 			var elemPos = $(this).offset().top-50;
 			var scroll = $(window).scrollTop();
 			var windowHeight = $(window).height();
@@ -144,10 +144,10 @@ function slideAnime(){
 				//左から右へ表示するクラスを取り除く
 				$(this).removeClass("slideAnimeLeftRight");
 				$(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
-				
+
 			}
 		});
-		
+
 		$('.rightAnime').each(function(){
 			var elemPos = $(this).offset().top-50;
 			var scroll = $(window).scrollTop();
@@ -161,7 +161,7 @@ function slideAnime(){
 				//右から左へ表示するクラスを取り除く
 				$(this).removeClass("slideAnimeRightLeft");
 				$(this).children(".rightAnimeInner").removeClass("slideAnimeLeftRight");
-				
+
 			}
 		});
     //====左右に動くアニメーションここまで===
@@ -195,16 +195,16 @@ function GlowAnimeControl() {
 // blurTriggerにblurというクラス名を付ける定義
 
 function BlurTextAnimeControl() {
-	$('.blurTrigger').each(function(){ //blurTriggerというクラス名が
+	$('.blurTrigger').each(function() { //blurTriggerというクラス名が
 		var elemPos = $(this).offset().top-50;//要素より、50px上の
 		var scroll = $(window).scrollTop();
 		var windowHeight = $(window).height();
 		if (scroll >= elemPos - windowHeight){
-		$(this).addClass('blur');// 画面内に入ったらblurというクラス名を追記
+			$(this).addClass('blur');// 画面内に入ったらblurというクラス名を追記
 		}else{
-		$(this).removeClass('blur');// 画面外に出たらblurというクラス名を外す
+			$(this).removeClass('blur');// 画面外に出たらblurというクラス名を外す
 		}
-		});
+	});
 }
 
 
@@ -212,13 +212,13 @@ function BlurTextAnimeControl() {
 /*印象編 6-7 スクロールするとヘッダー背景画像が拡大*/
 /*===========================================================*/
 $(window).scroll(function() {
-        var scroll = $(window).scrollTop();//スクロール値を定義
-    //header-imgの背景
+  var scroll = $(window).scrollTop();//スクロール値を定義
+  //header-imgの背景
 	$('#header-img').css({
-			transform: 'scale('+(100 + scroll/10)/100+')',//スクロール値を代入してscale1から拡大.scroll/10の値を小さくすると拡大値が大きくなる
-			top: -(scroll/50)  + "%",//スクロール値を代入してtopの位置をマイナスにずらす
-        });
-    });
+		transform: 'scale('+(100 + scroll/10)/100+')',//スクロール値を代入してscale1から拡大.scroll/10の値を小さくすると拡大値が大きくなる
+		top: -(scroll/50)  + "%",//スクロール値を代入してtopの位置をマイナスにずらす
+	});
+});
 
 /*===========================================================*/
 /*関数をまとめる*/
@@ -228,59 +228,56 @@ $(window).scroll(function() {
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
 	setFadeElement();//機能編  8-1-4ページ内にある指定の範囲内で下から出現の関数を呼ぶ
-    fadeAnime();// 印象編 4 最低限おぼえておきたい動きの関数を呼ぶ
-    slideAnime();// 印象編 8-2 テキストが流れるように出現（左から右）の関数を呼ぶ
+  fadeAnime();// 印象編 4 最低限おぼえておきたい動きの関数を呼ぶ
+  slideAnime();// 印象編 8-2 テキストが流れるように出現（左から右）の関数を呼ぶ
 	GlowAnimeControl();//印象編 8-17 テキストがほのかに光りながら出現の関数を呼ぶ
 	BlurTextAnimeControl();//印象編 8-9 テキストがじわっと出現の関数を呼ぶ
 });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 // ページが読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load',function(){
-    
-    $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述 
-    
+
+    $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+
     //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
 
-    $("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+    $("#splash").delay(1500).fadeOut('slow',function() {//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
     $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
-     
-    /* 印象編 8-17 テキストがほのかに光りながら出現*/  
-	//spanタグを追加する
-	var element = $(".glowAnime");
-	element.each(function () {
-		var text = $(this).text();
-		var textbox = "";
-		text.split('').forEach(function (t, i) {
-			if (t !== " ") {
-				if (i < 10) {
-					textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
-				} else {
-					var n = i / 10;
-					textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
-				}
-
-			} else {
-				textbox += t;
-			}
-		});
-		$(this).html(textbox);
-	});
-	GlowAnimeControl(); 
-	
+			/* 印象編 8-17 テキストがほのかに光りながら出現*/
+			//spanタグを追加する
+			var element = $(".glowAnime");
+			element.each(function() {
+				var text = $(this).text();
+				var textbox = "";
+				text.split('').forEach(function (t, i) {
+					if (t !== " ") {
+						if (i < 10) {
+							textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+						} else {
+							var n = i / 10;
+							textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+						}
+					} else {
+						textbox += t;
+					}
+				});
+				$(this).html(textbox);
+			});
+			GlowAnimeControl();
     });
     //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-  
+
     /*===========================================================*/
     /*機能編 4-2-5背景色が伸びる（中央から外）*/
     /*===========================================================*/
-    
+
     //=====ここから背景が伸びた後に動かしたいJSをまとめる
     $('.splashbg1').on('animationend', function() {
-	setFadeElement();//機能編 8-1-4ページ内にある指定の範囲内で下から出現の関数を呼ぶ
-    fadeAnime();// 印象編 4 最低限おぼえておきたい動きの関数を呼ぶ
-    slideAnime();//印象編 8-2 テキストが流れるように出現（左から右）の関数を呼ぶ
-	BlurTextAnimeControl();//印象編 8-9 テキストがじわっと出現の関数を呼ぶ  
-	});
+			setFadeElement();//機能編 8-1-4ページ内にある指定の範囲内で下から出現の関数を呼ぶ
+      fadeAnime();// 印象編 4 最低限おぼえておきたい動きの関数を呼ぶ
+      slideAnime();//印象編 8-2 テキストが流れるように出現（左から右）の関数を呼ぶ
+			BlurTextAnimeControl();//印象編 8-9 テキストがじわっと出現の関数を呼ぶ
+		});
     //=====ここまで背景が伸びた後に動かしたいJSをまとめる
 
 });// ここまでページが読み込まれたらすぐに動かしたい場合の記述
